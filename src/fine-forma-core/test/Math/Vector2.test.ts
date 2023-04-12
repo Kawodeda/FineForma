d2a3eeec6018e0cde4fe981920bfe6e3e502e8b0
Kawodeda <konserva023@gmail.com>
2023-04-13 01:58:41 +0700

    add Vector2 add operation commutative tests

--- src/fine-forma-core/test/Math/Vector2.test.ts
+++ src/fine-forma-core/test/Math/Vector2.test.ts
@@ -1,26 +1,42 @@
 import { assert } from "chai";
+import { expect } from "chai";
 import { suite, test } from "mocha";
+
 import { Vector2 } from "../../src/Math/Vector2";
 
 suite('Vector2', () => {
-    suite('add', () => {
+    suite('add returns the sum of vectors', () => {
         const testCases = [
-            { a: new Vector2(2, 2), b: new Vector2(1, 1), result: new Vector2(3, 3) },
-            { a: new Vector2(3, 3), b: new Vector2(2, 4), result: new Vector2(5, 7) },
-            { a: new Vector2(-1, 2.5), b: new Vector2(0, 0), result: new Vector2(-1, 2.5) },
-            { a: new Vector2(-9, 4.67), b: new Vector2(3.2, -4), result: new Vector2(-5.8, 0.67) },
-            { a: new Vector2(-100390, -3956703.56701), b: new Vector2(100390, 3956703.56701), result: new Vector2(0, 0) },
-            { a: new Vector2(0.76, 99), b: new Vector2(11, -5.8), result: new Vector2(11.76, 93.2) }
+            { a: new Vector2(2, 2), b: new Vector2(1, 1), expected: new Vector2(3, 3) },
+            { a: new Vector2(3, 3), b: new Vector2(2, 4), expected: new Vector2(5, 7) },
+            { a: new Vector2(-1, 2.5), b: new Vector2(0, 0), expected: new Vector2(-1, 2.5) },
+            { a: new Vector2(-9, 4.67), b: new Vector2(3.2, -4), expected: new Vector2(-5.8, 0.67) },
+            { a: new Vector2(-100390, -3956703.56701), b: new Vector2(100390, 3956703.56701), expected: new Vector2(0, 0) },
+            { a: new Vector2(0.76, 99), b: new Vector2(11, -5.8), expected: new Vector2(11.76, 93.2) }
         ];
 
-        testCases.forEach(({ a, b, result }) => {
+        testCases.forEach(({ a, b, expected }) => {
             test(`add (${a.x}, ${a.y}) to (${b.x}, ${b.y})`, () => {
-                assert.deepEqual(a.add(b), result);
+                expect(a.add(b)).to.be.deep.equal(expected);
             });
         });
     });
 
-    test('scale', () => {
-        assert.fail();
+    suite('add operation is commutative', () => {
+        const testCases = [
+            { a: new Vector2(2, 2), b: new Vector2(1, 1) },
+            { a: new Vector2(3, 3), b: new Vector2(2, 4) },
+            { a: new Vector2(-1, 2.5), b: new Vector2(0, 0) },
+            { a: new Vector2(-9, 4.67), b: new Vector2(3.2, -4) },
+            { a: new Vector2(-100390, -3956703.56701), b: new Vector2(100390, 3956703.56701) },
+            { a: new Vector2(0.76, 99), b: new Vector2(11, -5.8) },
+        ];
+
+        testCases.forEach(({ a, b }) => {
+            test(`add vectors (${a.x}, ${a.y}) and (${b.x}, ${b.y})`, () => {
+                expect(a.add(b)).to.be.deep.equal(b.add(a));
+            });
+        });
+    });
     });
 });
