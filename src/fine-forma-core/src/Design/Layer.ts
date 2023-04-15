715c3e162f3aec2737ae35a4fffe3931c07dc030
Kawodeda <konserva023@gmail.com>
2023-04-15 17:17:40 +0700

    add Design

---
+++ src/fine-forma-core/src/Design/Layer.ts
@@ -0,0 +1,20 @@
+import { Item } from './Items/Item';
+
+export class Layer {
+
+    private readonly _items: readonly Item[];
+    private readonly _zIndex: number;
+
+    constructor(items: readonly Item[], zIndex: number) {
+        this._items = items;
+        this._zIndex = zIndex;
+    }
+
+    get items(): readonly Item[] {
+        return this._items;
+    }
+
+    get zIndex(): number {
+        return this._zIndex;
+    }
+}
