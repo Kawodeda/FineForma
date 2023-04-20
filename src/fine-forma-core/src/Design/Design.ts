04ac6ec1123b33db30a7c8ee60fccbbf7275b503
Kawodeda <konserva023@gmail.com>
2023-04-21 00:10:27 +0700

    make design layers ordered by z index

--- src/fine-forma-core/src/Design/Design.ts
+++ src/fine-forma-core/src/Design/Design.ts
@@ -5,10 +5,17 @@ export class Design {
     private readonly _layers: readonly Layer[];
 
     constructor(layers: readonly Layer[]) {
-        this._layers = layers;
+        this._layers = this._orderByZIindex(layers);
     }
 
     get layers(): readonly Layer[] {
         return this._layers;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    private _orderByZIindex(layers: readonly Layer[]): readonly Layer[] {
+        return [...layers].sort(
+            (a, b) => a.zIndex - b.zIndex
+        );
     }
 }
