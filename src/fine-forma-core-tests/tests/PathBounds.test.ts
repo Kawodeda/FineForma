d34cd5efa466e020e968ebf3db304f688a6a8331
Kawodeda <konserva023@gmail.com>
2023-05-09 19:06:10 +0700

    add arc bounds tests

--- src/fine-forma-core-tests/tests/PathBounds.test.ts
+++ src/fine-forma-core-tests/tests/PathBounds.test.ts
@@ -53,3 +53,58 @@ suite('Line segment bounds', () => {
         });
     });
 });
+
+suite('Arc segment bounds', () => {
+    const testCases = [
+        {
+            arc: new ArcSegment(new Vector2(0, 0), new Vector2(40, 0), new Vector2(20, 20), 0),
+            expected: new Rectangle(new Vector2(0, 0), new Vector2(40, 20))
+        },
+        {
+            arc: new ArcSegment(new Vector2(40, 20), new Vector2(0, 20), new Vector2(20, 20), 0),
+            expected: new Rectangle(new Vector2(0, 0), new Vector2(40, 20))
+        },
+        {
+            arc: new ArcSegment(new Vector2(0, 20), new Vector2(0, 0), new Vector2(40, 10), 0),
+            expected: new Rectangle(new Vector2(0, 0), new Vector2(40, 20))
+        },
+        {
+            arc: new ArcSegment(new Vector2(40, 0), new Vector2(40, 20), new Vector2(40, 10), 0),
+            expected: new Rectangle(new Vector2(0, 0), new Vector2(40, 20))
+        },
+        {
+            arc: new ArcSegment(new Vector2(40, 0), new Vector2(0, 0), new Vector2(20, 20), 0),
+            expected: new Rectangle(new Vector2(0, -20), new Vector2(40, 0))
+        },
+        {
+            arc: new ArcSegment(new Vector2(40, 0), new Vector2(-18, 38), new Vector2(30, 40), 0),
+            expected: new Rectangle(new Vector2(-21.305, -24.0637), new Vector2(40, 38))
+        },
+        {
+            arc: new ArcSegment(new Vector2(40, 0), new Vector2(51, -29), new Vector2(30, 40), 0),
+            expected: new Rectangle(new Vector2(40, -29), new Vector2(51, 0))
+        },
+        {
+            arc: new ArcSegment(new Vector2(40, 0), new Vector2(15, 0), new Vector2(30, 0), 0),
+            expected: new Rectangle(new Vector2(15, 0), new Vector2(40, 0))
+        },
+        {
+            arc: new ArcSegment(new Vector2(40, 0), new Vector2(20, -13), new Vector2(30, 0), 0),
+            expected: new Rectangle(new Vector2(20, -13), new Vector2(40, 0))
+        },
+        {
+            arc: new ArcSegment(new Vector2(40, 0), new Vector2(40, -139), new Vector2(30, 50), 0),
+            expected: new Rectangle(new Vector2(40, -139), new Vector2(81.7, 0))
+        },
+        {
+            arc: new ArcSegment(new Vector2(40, -139), new Vector2(40, 0), new Vector2(30, 50), 0),
+            expected: new Rectangle(new Vector2(-1.7, -139), new Vector2(40, 0))
+        }
+    ];
+
+    testCases.forEach(({ arc, expected }, index) => {
+        test(`arc #${index + 1}`, () => {
+            expect(arc.bounds.equals(expected)).to.be.true;
+        });
+    });
+});
