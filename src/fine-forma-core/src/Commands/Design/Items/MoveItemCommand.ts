bed083cf1986d7086751dfdc40e28583c5f9751d
Kawodeda <konserva023@gmail.com>
2023-05-09 14:58:23 +0700

    implement move item command

--- src/fine-forma-core/src/Commands/Design/Items/MoveItemCommand.ts
+++ src/fine-forma-core/src/Commands/Design/Items/MoveItemCommand.ts
@@ -1,14 +1,28 @@
-import { Design, Item } from '../../../Design';
+import { Design, Item, Layer } from '../../../Design';
 import { Vector2 } from '../../../Math';
 import { ItemCommand } from './ItemCommand';
 
 export class MoveItemCommand extends ItemCommand {
     
+    private readonly _shift: Vector2;
+
     constructor(item: Item, shift: Vector2) {
-        
+        super(item);
+
+        this._shift = shift;
     }
 
     override execute(design: Design): Promise<Design> {
-        throw new Error('Method not implemented.');
+        const layer = design.getLayerOf(this._item);
+        const newLayer = new Layer(
+            layer.items.update(
+                this._item, 
+                this._item.setPosition(this._item.position.add(this._shift))),
+            layer.zIndex
+        );
+        
+        return Promise.resolve(
+            new Design(design.layers.update(layer, newLayer))
+        );
     }
 }
