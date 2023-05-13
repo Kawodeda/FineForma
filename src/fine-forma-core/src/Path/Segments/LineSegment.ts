8aac08a7a5229d7270a543213a3ee54a4eb92331
Kawodeda <konserva023@gmail.com>
2023-05-13 16:52:34 +0700

    move line bounds calculation to caonstructor

--- src/fine-forma-core/src/Path/Segments/LineSegment.ts
+++ src/fine-forma-core/src/Path/Segments/LineSegment.ts
@@ -1,11 +1,19 @@
 import { Segment } from './Segment';
 import { IPathBuilder } from './../IPathBuilder';
-import { Bounds } from '../../Math';
+import { Bounds, Vector2 } from '../../Math';
 
 export class LineSegment extends Segment {
 
+    private readonly _bounds: Bounds;
+
+    constructor(start: Vector2, end: Vector2) {
+        super(start, end);
+
+        this._bounds = Bounds.from([this.start, this.end]);
+    }
+
     override get bounds(): Bounds {
-        return Bounds.from([this.start, this.end]);
+        return this._bounds;
     }
 
     override addToPath(pathBuilder: IPathBuilder): void {
