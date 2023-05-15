5c96dd852a694679362ee178c16f9a31739657c4
Kawodeda <konserva023@gmail.com>
2023-05-15 19:22:44 +0700

    add selection

--- src/fine-forma-core/src/Viewer.ts
+++ src/fine-forma-core/src/Viewer.ts
@@ -1,6 +1,7 @@
 import { ICommand, IExecutionContext } from './Commands/Interfaces';
 import { Design } from './Design';
 import { IRenderer, IDesignContext, IViewportContext, IRendererFactory } from './Rendering';
+import { Selection } from './Selection';
 import { Viewport } from './Viewport';
 
 export class Viewer implements IDesignContext, IViewportContext, IExecutionContext {
@@ -9,11 +10,13 @@ export class Viewer implements IDesignContext, IViewportContext, IExecutionConte
 
     private _design: Design;
     private _viewport: Viewport;
+    private _selection: Selection;
 
     constructor(design: Design, viewport: Viewport, rendererFactory: IRendererFactory) {
         this._design = design;
         this._viewport = viewport;
         this._renderer = rendererFactory.create(this, this);
+        this._selection = Selection.empty;
     }
 
     get renderer(): IRenderer {
@@ -28,15 +31,34 @@ export class Viewer implements IDesignContext, IViewportContext, IExecutionConte
         return this._viewport;
     }
 
+    get selection(): Selection {
+        return this._selection;
+    }
+
     set design(design: Design) {
         this._design = design;
+        this._selection = this._selection.remove(
+            this._selection.items.filter(item => !design.contains(item))
+        );
     }
 
     set viewport(viewport: Viewport) {
         this._viewport = viewport;
     }
 
+    set selection(selection: Selection) {
+        this._validateSelection(selection, this.design);
+
+        this._selection = selection;
+    }
+
     async execute(command: ICommand): Promise<void> {
         await command.execute(this);
+    }
+
+    private _validateSelection(selection: Selection, design: Design): void {
+        if (selection.items.some(item => !design.contains(item))) {
+            throw new Error('The selection contains items that are missing in the design');
+        }
     }
 }
