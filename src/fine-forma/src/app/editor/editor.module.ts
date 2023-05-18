dae0aa78307ea9a9f86f80bcc3ed2c786500556b
Kawodeda <konserva023@gmail.com>
2023-05-19 00:34:39 +0700

    add editor module

---
+++ src/fine-forma/src/app/editor/editor.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+
+import { EditorComponent } from './editor.component';
+import { ViewerComponent } from './viewer/viewer/viewer.component';
+
+@NgModule({
+    imports: [],
+    exports: [],
+    declarations: [EditorComponent, ViewerComponent],
+    providers: [],
+})
+export class EditorModule { }
