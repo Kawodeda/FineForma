b61cb266255f5998393947da08ba9e7c5d4c9c38
Kawodeda <konserva023@gmail.com>
2023-05-19 18:55:02 +0700

    make viewer canvas occupy all available space

--- src/fine-forma/src/app/editor/viewer/editor-viewer.component.ts
+++ src/fine-forma/src/app/editor/viewer/editor-viewer.component.ts
@@ -12,13 +12,23 @@ export class ViewerComponent implements AfterViewInit {
     @ViewChild('mainCanvas') canvas: ElementRef<HTMLCanvasElement> | undefined;
 
     private readonly _renderingService: IViewerRenderingService;
+    private readonly _canvasResizeObserver: ResizeObserver;
 
     constructor(@Inject(VIEWER_RENDERING_SERVICE) renderingService: IViewerRenderingService) {
         this._renderingService = renderingService;
+        this._canvasResizeObserver = new ResizeObserver(entries => this._onCanvasResized(entries));
+    }
+
+    private get _canvas(): HTMLCanvasElement {
+        if (this.canvas?.nativeElement == null) {
+            throw new Error('Unable to get canvas');
+        }
+
+        return this.canvas.nativeElement;
     }
 
     private get _context(): CanvasRenderingContext2D {
-        const ctx = this.canvas?.nativeElement.getContext('2d');
+        const ctx = this._canvas.getContext('2d');
         if(ctx == null) {
             throw new Error('Unable to get canvas rendering context');
         }
@@ -33,6 +43,24 @@ export class ViewerComponent implements AfterViewInit {
             requestAnimationFrame(redrawViewer);
         }
 
+        this._canvasResizeObserver.observe(this._canvas, { box: 'content-box' });
         redrawViewer();
+    }
+
+    private _onCanvasResized(entries: ResizeObserverEntry[]): void {
+        const canvas = entries[0];
+        if (canvas == null || canvas.contentBoxSize?.at(0) == null) {
+            throw new Error('Canvas resize processing falied');
+        }
+
+        this._setCanvasSize(
+            canvas.contentBoxSize[0].inlineSize, 
+            canvas.contentBoxSize[0].blockSize
+        );
+    }
+
+    private _setCanvasSize(width: number, height: number): void {
+        this._canvas.width = width;
+        this._canvas.height = height;
     }
 }
