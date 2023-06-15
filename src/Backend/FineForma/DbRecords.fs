816bbc91d477b29e5f08e1e0ab878f29c3ee081a
Kawodeda <konserva023@gmail.com>
2023-06-16 00:10:47 +0700

    add db records

---
+++ src/Backend/FineForma/DbRecords.fs
@@ -0,0 +1,52 @@
+module FineForma.DbRecords
+
+open System
+
+[<CLIMutable>]
+type User = {
+    Id: int
+    Username: string
+    Password: byte array
+    RegistrationDate: DateTime
+}
+
+[<CLIMutable>]
+type StoredResource = {
+    Id: int
+    StorageId: string
+    Name: string
+    Thumbnail: byte array
+}
+
+[<CLIMutable>]
+type Design = {
+    Id: int
+    Source: int
+    LastEdited: DateTime
+}
+
+[<CLIMutable>]
+type SavedDesign = {
+    Id: int
+    User: int
+    Design: int
+}
+
+[<CLIMutable>]
+type AssetType = {
+    Id: int
+    Title: string
+}
+
+[<CLIMutable>]
+type Asset = {
+    Id: int
+    Source: int
+    Type: int
+}
+
+[<CLIMutable>]
+type SavedAsset = {
+    User: int
+    Asset: int
+}
