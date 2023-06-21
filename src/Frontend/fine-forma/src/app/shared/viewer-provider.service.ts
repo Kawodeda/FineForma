2b3cadf6aa641a2ac1ca28c3cd705c872e571a89
Kawodeda <konserva023@gmail.com>
2023-06-21 11:56:18 +0700

    change default design

--- src/Frontend/fine-forma/src/app/shared/viewer-provider.service.ts
+++ src/Frontend/fine-forma/src/app/shared/viewer-provider.service.ts
@@ -37,7 +37,15 @@ import {
     ResizeGripsRenderer,
     ResizeInputHandler,
     SelectionDebugRenderer,
-    IRenderer
+    IRenderer,
+    Path,
+    ClosedPath,
+    CubicBezierSegment,
+    LineSegment,
+    ClosedShapeItem,
+    Transform,
+    PathControls,
+    ClosedShapeStyle
 } from 'fine-forma-core';
 
 import { IViewerProvider } from './i-viewer-provider';
@@ -106,20 +114,27 @@ export class ViewerProvider implements IViewerProvider {
     private _createDesign(): Design {
         return new Design([
             new Layer([
-                createCircle(200, 300, 90).setFill(Brushes.green()).build(),
+                createCircle(100, 200, 90)
+                    .setFill(Brushes.green())
+                    .setStroke(new Pen(Brushes.black(), 0))
+                    .build(),
                 createRectangle(400, 200, 150, 80)
                     .setFill(Brushes.lavender())
-                    .setStroke(new Pen(Brushes.red(), 4, new DashSettings([9, 4])))
+                    .setStroke(new Pen(Brushes.red(), 4, new DashSettings([10, 10])))
                     .build(),
-                createImage(600, 600, 200, 200, 'masyunya3').build(),
-                createImage(800, 200, 200, 200, 'sima2').build()
-            ], 1),
-            new Layer([
-                createRectangle(50, 50, 100, 100).build(),
-                createRectangle(950, 950, 100, 100).build(),
-                createRectangle(950, 50, 100, 100).build(),
-                createRectangle(50, 950, 100, 100).build()
-            ], 2)
+                new ClosedShapeItem(
+                    new Vector2(400, 500),
+                    Transform.createIdentity(),
+                    new PathControls(this._pathApple()),
+                    new ClosedShapeStyle(new Pen(Brushes.black(), 0), Brushes.red())
+                ),
+                createImage(700, 600, 200, 200, 'masyunya')
+                .setBorder(new Pen(Brushes.black(), 0))
+                .build(),
+                createImage(800, 200, 200, 200, 'sima2')
+                .setBorder(new Pen(Brushes.black(), 0))
+                .build()
+            ], 1)
         ]);
     }
 
@@ -172,5 +187,27 @@ export class ViewerProvider implements IViewerProvider {
                 }
             }
         );
+    }
+
+    private _pathApple(): Path {
+        return new ClosedPath([
+            new CubicBezierSegment(new Vector2(110, 40), new Vector2(104, 53.3), new Vector2(101.1, 59.3), new Vector2(93.4, 71)),
+            new CubicBezierSegment(new Vector2(93.4, 71), new Vector2(82.6, 87.4), new Vector2(67.4, 107.9), new Vector2(48.5, 108.1)),
+            new CubicBezierSegment(new Vector2(48.5, 108.1), new Vector2(31.7, 108.3), new Vector2(27.4, 97.2), new Vector2(4.7, 97.3)),
+            new CubicBezierSegment(new Vector2(4.7, 97.3), new Vector2(-18, 97.4), new Vector2(-22.8, 108.3), new Vector2(-39.6, 108.1)),
+            new CubicBezierSegment(new Vector2(-39.6, 108.1), new Vector2(-58.5, 107.9), new Vector2(-72.9, 89.4), new Vector2(-83.7, 73)),
+            new CubicBezierSegment(new Vector2(-83.7, 73), new Vector2(-113.9, 27), new Vector2(-117.1, -26.9), new Vector2(-98.4, -55.6)),
+            new CubicBezierSegment(new Vector2(-98.4, -55.6), new Vector2(-85.2, -76), new Vector2(-64.3, -87.9), new Vector2(-44.6, -87.9)),
+            new CubicBezierSegment(new Vector2(-44.6, -87.9), new Vector2(-24.6, -87.9), new Vector2(-12.1, -76.9), new Vector2(4.5, -76.9)),
+            new CubicBezierSegment(new Vector2(4.5, -76.9), new Vector2(20.5, -76.9), new Vector2(30.3, -87.9), new Vector2(53.4, -87.9)),
+            new CubicBezierSegment(new Vector2(53.4, -87.9), new Vector2(70.9, -87.9), new Vector2(89.4, -78.4), new Vector2(102.6, -61.9)),
+            new CubicBezierSegment(new Vector2(102.6, -61.9), new Vector2(59.3, -38.2), new Vector2(66.3, 23.5), new Vector2(110, 40)),
+            new LineSegment(new Vector2(110, 40), new Vector2(110, 40)),
+            new CubicBezierSegment(new Vector2(35.8, -106.2), new Vector2(44.2, -117), new Vector2(50.6, -132.2), new Vector2(48.3, -147.8)),
+            new CubicBezierSegment(new Vector2(48.3, -147.8), new Vector2(34.6, -146.9), new Vector2(18.5, -138.1), new Vector2(9.2, -126.7)),
+            new CubicBezierSegment(new Vector2(9.2, -126.7), new Vector2(0.7, -116.4), new Vector2(-6.3, -101.1), new Vector2(-3.6, -86.2)),
+            new CubicBezierSegment(new Vector2(-3.6, -86.2), new Vector2(11.3, -85.7), new Vector2(26.8, -94.7), new Vector2(35.8, -106.2)),
+            new LineSegment(new Vector2(35.8, -106.2), new Vector2(35.8, -106.2))
+        ]);
     }
 }
